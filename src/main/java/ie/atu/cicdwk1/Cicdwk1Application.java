dcb026b2053c0da327347ee531238d92db6579bb
EvanFarrell-STUDENT <g00326940@atu.ie>
2026-09-16 14:27:56 +0100

    initial spring boot catalog service

---
+++ src/main/java/ie/atu/cicdwk1/Cicdwk1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdwk1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicdwk1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicdwk1Application.class, args);
+    }
+
+}
