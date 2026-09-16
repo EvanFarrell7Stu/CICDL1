1277cb638a115fcc314d705874e9bff4806b6923
EvanFarrell-STUDENT <g00326940@atu.ie>
2026-09-16 15:23:52 +0100

    service added

--- src/main/java/ie/atu/cicdwk1/Service/ProductService.java
+++ src/main/java/ie/atu/cicdwk1/Service/ProductService.java
@@ -1,5 +1,25 @@
 package ie.atu.cicdwk1.Service;
 
-public class ProductService {
+import ie.atu.cicdwk1.model.Product;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+private final List<Product> products= new ArrayList<>();
+private long nextId = 1;
+
+public List<Product> getAll() {
+    return products;
+}
+
+@PostMapping
+public Product create(@RequestBody Product product) {
+    product.setId(nextId++);
+    products.add(product);
+    return product;
+}
 }
