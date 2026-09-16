4aea2e9a7c8a3a660048e780630aeb2a49f82a1b
EvanFarrell-STUDENT <g00326940@atu.ie>
2026-09-16 15:15:52 +0100

    service and model created

---
+++ src/main/java/ie/atu/cicdwk1/Service/ProductService.java
@@ -0,0 +1,5 @@
+package ie.atu.cicdwk1.Service;
+
+public class ProductService {
+
+}
--- src/main/java/ie/atu/cicdwk1/model/Product.java
+++ src/main/java/ie/atu/cicdwk1/model/Product.java
@@ -1,5 +1,17 @@
 package ie.atu.cicdwk1.model;
 
-public class Product {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private BigDecimal price;
 }
+
