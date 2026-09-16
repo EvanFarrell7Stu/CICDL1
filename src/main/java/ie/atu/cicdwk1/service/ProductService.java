a92fccc1bfd523dd512ee517491f4388c43ba9d8
EvanFarrell-STUDENT <g00326940@atu.ie>
2026-09-16 14:54:07 +0100

    commit service

---
+++ src/main/java/ie/atu/cicdwk1/service/ProductService.java
@@ -0,0 +1,25 @@
+package ie.atu.cicdwk1.service;
+
+import ie.atu.cicdwk1.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+  private final List<Product> products =new ArrayList<>();
+  private long nextId=1;
+
+  public List<Product> getAll(){
+      return products;
+  }
+
+  public Product create(Product product){
+      product.setId(nextId++);
+      products.add(product);
+      return product;
+  }
+
+
+}
